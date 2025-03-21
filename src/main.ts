f3c8006087fffea27424d0a35162181987316f09
Matianxing <450086055@qq.com>
2025-03-21 10:24:53 +0800

    style: 样式部分结构搭建

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import { getPlatformConfig } from "./config";
+
+// 引入重置样式
+import "./style/reset.scss";
+import "./style/index.scss";
+import "./style/tailwind.css";
+import "element-plus/dist/index.css";
 
 const app = createApp(App);
 
 getPlatformConfig(app).then(config => {
   console.log(config);
+  app.use(createPinia());
   app.use(router);
   app.mount("#app");
 });
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bg_color: "var(--el-bg-color)",
+        primary: "var(--el-color-primary)",
+        text_color_primary: "var(--el-text-color-primary)",
+        text_color_regular: "var(--el-text-color-regular)"
+      }
+    }
+  },
+  plugins: []
+};
